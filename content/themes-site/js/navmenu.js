8be8fe7f863b35e4a5fe78a3702bd810941cea91
Exodysseus <107181401+Exodysseus@users.noreply.github.com>
2023-05-31 02:20:36 +0930

    Primary Site Content
    Signed-off-by: Exodysseus <107181401+Exodysseus@users.noreply.github.com>

---
+++ content/themes-site/js/navmenu.js
@@ -0,0 +1,26 @@
+var acc = document.getElementsByClassName("nav-linkAccordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
